9dba165bfd0851411e9e9eb3c4d7fee033ed3c62
JordanRBurke <35742067+JordanRBurke@users.noreply.github.com>
2019-02-07 11:20:56 -0500

    Finished register activity firebase code for registering users to the database. Beginning test to see if registration is working properly.

--- app/src/main/java/com/example/jordanrburke/shelbycountyparksapp/RegisterActivity.java
+++ app/src/main/java/com/example/jordanrburke/shelbycountyparksapp/RegisterActivity.java
@@ -1,11 +1,22 @@
 package com.example.jordanrburke.shelbycountyparksapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -15,11 +26,52 @@ public class RegisterActivity extends AppCompatActivity {
     protected EditText nameRegisterText;
     @BindView(R.id.password_edit_register)
     protected EditText passwordRegisterText;
+    @BindView(R.id.register_button_register)
+    protected Button registerButton;
+    @BindView(R.id.sign_in_button_register)
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
+
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+            finish();
+        }
+
+    }
+
+    @OnClick(R.id.register_button_register)
+    protected void registerButtonClicked() {
+        final String email = emailRegisterText.getText().toString();
+        final String password = passwordRegisterText.getText().toString();
+
+        try {
+            if (password.length() > 0 && email.length() > 0) {
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(RegisterActivity.this, "Registration Failed", Toast.LENGTH_LONG).show();
+                                    Log.v("error", task.getResult().toString());
+
+                                } else {
+                                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                }
+                            }
+                        });
+            } else {
+                Toast.makeText(this, "Fill All Fields", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
