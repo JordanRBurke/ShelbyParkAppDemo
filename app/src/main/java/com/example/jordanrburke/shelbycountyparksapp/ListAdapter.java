ff2c70c91904878e4747b156534b6ce0a8105bf7
JordanRBurke <35742067+JordanRBurke@users.noreply.github.com>
2019-02-08 09:12:41 -0500

    Added adapter for recycler view which was included in the pool selection fragment. List did not show up but I expect it's because of the xml file that it is viewing.

--- app/src/main/java/com/example/jordanrburke/shelbycountyparksapp/ListAdapter.java
+++ app/src/main/java/com/example/jordanrburke/shelbycountyparksapp/ListAdapter.java
@@ -1,4 +1,50 @@
 package com.example.jordanrburke.shelbycountyparksapp;
 
-public class ListAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder> {
+    private String [] mDataset;
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        TextView titleSelection = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.test_view_selection, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(titleSelection);
+        return vh;
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        holder.eventName.setText(mDataset[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView eventName;
+        public MyViewHolder(TextView v) {
+            super(v);
+            eventName = v;
+        }
+    }
+
+    public ListAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+
+
 }
