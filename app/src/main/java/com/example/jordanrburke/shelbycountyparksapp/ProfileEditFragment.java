32e70992310661bd678d12e5550678fba4a254c1
JordanRBurke <35742067+JordanRBurke@users.noreply.github.com>
2019-02-07 19:41:03 -0500

    Changing theme of application and changing things around in the MainActivity.

---
+++ app/src/main/java/com/example/jordanrburke/shelbycountyparksapp/ProfileEditFragment.java
@@ -0,0 +1,38 @@
+package com.example.jordanrburke.shelbycountyparksapp;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProfileEditFragment extends Fragment {
+
+
+    public ProfileEditFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile_edit, container, false);
+    }
+
+    public static ProfileEditFragment newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        ProfileEditFragment fragment = new ProfileEditFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+}
